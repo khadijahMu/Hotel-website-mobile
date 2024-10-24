a566165ec598bb620815fc7564b9d69e866ad781
khadijah <khadeejahmuazu@gmail.com>
2024-10-24 23:21:03 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.querySelector('.show-more-btn').addEventListener('click', function() {
     const additionalReviews = document.querySelector('.additional-reviews');
     
-    // Check if the reviews are currently visible
+    
     if (additionalReviews.style.display === 'none' || additionalReviews.style.display === '') {
         additionalReviews.style.display = 'block';  // Show the reviews
     } else {
